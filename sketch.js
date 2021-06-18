6a08ccdc2e667b2b0477a93dc3eacd4386271c5d
Protik Sarkar <protik.sarkar@gmail.com>
2021-06-18 03:19:26 +0200

    displayWidth

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
-function make2DArray(cols, rows) {
-    let arr = new Array(cols);
+function make2DArray(columns, rows) {
+    let arr = new Array(columns);
     for (let i = 0; i < arr.length; i++) {
       arr[i] = new Array(rows);
     }
@@ -8,17 +8,17 @@ function make2DArray(cols, rows) {
   }
   
   let grid;
-  let cols;
+  let columns;
   let rows;
   let resolution = 10;
   
   function setup() {
-    createCanvas(600, 400);
-    cols = width / resolution;
+    createCanvas(displayWidth, displayHeight,400);
+    columns = width / resolution;
     rows = height / resolution;
   
-    grid = make2DArray(cols, rows);
-    for (let i = 0; i < cols; i++) {
+    grid = make2DArray(columns, rows);
+    for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
         grid[i][j] = floor(random(2));
       }
@@ -29,7 +29,7 @@ function make2DArray(cols, rows) {
     //frameRate(2)
     background(0);
   
-    for (let i = 0; i < cols; i++) {
+    for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
         let x = i * resolution;
         let y = j * resolution;
@@ -41,15 +41,15 @@ function make2DArray(cols, rows) {
       }
     }
   
-    let next = make2DArray(cols, rows);
+    let next = make2DArray(columns, rows);
   
     // Compute next based on grid
-    for (let i = 0; i < cols; i++) {
+    for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
         let state = grid[i][j];
         // Count live neighbors!
         let sum = 0;
-        let neighbors = countNeighbors(grid, i, j);
+        let neighbors = neighbours(grid, i, j);
   
         if (state == 0 && neighbors == 3) {
           next[i][j] = 1;
@@ -69,11 +69,11 @@ function make2DArray(cols, rows) {
   }
   
   
-  function countNeighbors(grid, x, y) {
+  function neighbours(grid, x, y) {
     let sum = 0;
     for (let i = -1; i < 2; i++) {
       for (let j = -1; j < 2; j++) {
-        let col = (x + i + cols) % cols;
+        let col = (x + i + columns) % columns;
         let row = (y + j + rows) % rows;
         sum += grid[col][row];
       }
